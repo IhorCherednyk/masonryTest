cc33dbf7c7201826275192b16e214926a2daff00
Ihor <ihor_cherednyk@mail.ru>
2016-10-04 19:51:05 +0300

    asdasddasdasda

--- js/masonry.initial.js
+++ js/masonry.initial.js
@@ -19,37 +19,68 @@
 
 // VARIANT NUMBER 2
 
-var $grid = $('.grid').imagesLoaded( function() {
-  $grid.masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer'
-  }); 
-});
+// var $grid = $('.grid').imagesLoaded( function() {
+// 	$grid.masonry({
+// 		itemSelector: '.grid-item',
+// 		percentPosition: true,
+// 		columnWidth: '.grid-sizer'
+// 	}); 
+// });
 
-$('.bit').click(function(){
-	alert('click')
-	$grid.masonry('layout');
-})
+// $('.bit').click(function(){
+// 	alert('click')
+// 	$grid.masonry('layout');
+// })
 
+var imagePreloadQuantity = 10,
+allImages,
+allImagesQuantity
 
-var $container = $('.grid');
-$container.imagesLoaded(function(){
-    $container.masonry({
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer'
-    });
-    $('.grid-item img').addClass('not-loaded');
-    $('.grid-item img.not-loaded').lazyload({
-        effect: 'fadeIn',
-        load: function() {
-            console.log($(this))
-            $(this).removeClass("not-loaded");
-            $container.masonry('layout');
-        }
-    });
-    $('.item img.not-loaded').trigger('scroll');
-});
+function masonryImagePreloader(){
+	
+	allImages = $('img.no-loadeds');
+	allImagesQuantity = allImages.length;
+	allImages.each(function(key,value){
+		if(key < imagePreloadQuantity && $(this).hasClass('no-loadeds')){
+			var img = new Image();
+			img.src = $(value).data('original');
+			$(this).attr('src', $(value).data('original')).removeClass('no-loadeds') //Перезаписывам src
+
+		}
+		else if (key != 0 && key >= imagePreloadQuantity && $(this).hasClass('no-loadeds')) {
+			var $grid = $('.grid').imagesLoaded( function() {
+				$grid.masonry({
+					itemSelector: '.grid-item',
+					percentPosition: true,
+					columnWidth: '.grid-sizer'
+				}); 
+			});
+			masonryImagePreloader(allImages)
+		}else {
+			return false
+
+		}
+	})
+}
+masonryImagePreloader(allImages);
+
+// var $container = $('.grid');
+// $container.imagesLoaded(function(){
+//     $container.masonry({
+//         itemSelector: '.item',
+//         columnWidth: '.grid-sizer'
+//     });
+//     $('.item img').addClass('not-loaded');
+//     $('.item img.not-loaded').lazyload({
+//         effect: 'fadeIn',
+//         load: function() {
+//             // Disable trigger on this image
+//             $(this).removeClass("not-loaded");
+//             $container.masonry('reload');
+//         }
+//     });
+//     $('.item img.not-loaded').trigger('scroll');
+// });
 
 
 
